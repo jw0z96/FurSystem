d5981df36375496f04526ee4b28ffaf0ac5db5dd
Joe Withers <joewithers96@gmail.com>
2018-01-31 21:41:54 +0000

    removed label from renderable node & deregistering old meshes on reconnect

--- src/MeshRendererModel.cpp
+++ src/MeshRendererModel.cpp
@@ -8,16 +8,8 @@
 #include "MeshData.h"
 #include "Viewport.h"
 
-MeshRendererModel::MeshRendererModel() :
-	m_label(new QLabel(""))
+MeshRendererModel::MeshRendererModel()
 {
-	m_label->setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
-	QFont f = m_label->font();
-	// f.setBold(true);
-	// f.setItalic(true);
-	m_label->setFont(f);
-	m_label->installEventFilter(this);
-
 	m_meshRenderable = std::make_shared<MeshRenderable>();
 }
 
@@ -43,11 +35,6 @@ unsigned int MeshRendererModel::nPorts(PortType portType) const
 
 bool MeshRendererModel::eventFilter(QObject *object, QEvent *event)
 {
-	if (object == m_label)
-	{
-		// std::cout<<"MeshRendererModel::eventFilter\n";
-	}
-
 	return false;
 }
 
@@ -66,14 +53,16 @@ void MeshRendererModel::setInData(std::shared_ptr<NodeData> nodeData, PortIndex)
 {
 	_nodeData = nodeData;
 
-	if (_nodeData)
+	if (_nodeData) // connected
 	{
+		// deregister the current renderable from the viewport
+		Viewport::getInstance()->deregisterRenderable(m_meshRenderable);
 		auto d = std::static_pointer_cast<MeshData>(_nodeData);
 		m_meshRenderable = std::make_shared<MeshRenderable>(d->mesh());
 		std::cout<<"register the renderable with the viewport\n";
 		Viewport::getInstance()->registerRenderable(m_meshRenderable);
 	}
-	else
+	else // disconnected
 	{
 		std::cout<<"deregister the renderable from the viewport\n";
 		Viewport::getInstance()->deregisterRenderable(m_meshRenderable);
