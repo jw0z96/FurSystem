f94defa32686e6958fe226650e389d9000eba3ea
Joe Withers <joewithers96@gmail.com>
2018-02-09 00:45:58 +0000

    added to references.bib & calculating triangle area

--- src/DistributorModel.cpp
+++ src/DistributorModel.cpp
@@ -102,9 +102,21 @@ void DistributorModel::distribute()
 
 	for (auto face : m_mesh.m_faces)
 	{
-		glm::vec3 basePos = m_mesh.m_vertexPositions[face.pos[0]];
-		basePos += m_mesh.m_vertexPositions[face.pos[1]];
-		basePos += m_mesh.m_vertexPositions[face.pos[2]];
+		glm::vec3 posA = m_mesh.m_vertexPositions[face.pos[0]];
+		glm::vec3 posB = m_mesh.m_vertexPositions[face.pos[1]];
+		glm::vec3 posC = m_mesh.m_vertexPositions[face.pos[2]];
+
+		glm::vec3 e1 = posB - posA;
+		glm::vec3 e2 = posC - posA;
+		glm::vec3 e3 = glm::cross(e1, e2);
+
+		float triArea = 0.5 * sqrt(e3.x * e3.x + e3.y * e3.y + e3.z * e3.z);
+
+		std::cout<<"triArea: "<<triArea<<"\n";
+
+		glm::vec3 basePos = posA;
+		basePos += posB;
+		basePos += posC;
 		basePos /= 3.0;
 
 		glm::vec3 baseNorm = m_mesh.m_vertexNormals[face.normal[0]];
